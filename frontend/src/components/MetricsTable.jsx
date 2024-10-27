31b5723148b089eb838461bbf8747bb7a9a19c93
juanmrtnz <juanantoniomsn@gmail.com>
2024-10-27 15:45:13 +0000

    added overflow to metrics table

--- frontend/src/components/MetricsTable.jsx
+++ frontend/src/components/MetricsTable.jsx
@@ -34,25 +34,24 @@ export default function MetricsTable({ metrics }) {
                 <div className='flex items-center text-gray-400 text-xs font-light'>VALUE</div>
                 <div className='flex items-center text-gray-400 text-xs font-light'>DATE</div>
             </div>
-            <div>
-
+            <div className='max-h-96 overflow-y-auto'>
+                {metrics.map((metric =>
+                    <div key={`${metric.type}-${metric.id}`}className={`mt-2 pl-20 py-1
+                        grid grid-cols-3 border border-gray-200 rounded shadow`}
+                    >
+                        <div className='flex items-center space-x-2'>
+                            <span 
+                                className={`w-3 h-3 flex rounded-full ${getDotColor(metric.type)}`}
+                            ></span>
+                            <span>{metric.type}</span>
+                        </div>
+                        <div className='flex items-center'>{metric.value}</div>
+                        <div className='flex items-center text-xs'>
+                            {formatDate(metric.date)}
+                        </div>
+                    </div>
+                ))}
             </div>
-            {metrics.map((metric =>
-                <div key={`${metric.type}-${metric.id}`}className={`mt-2 pl-20 py-1
-                    grid grid-cols-3 border border-gray-200 rounded shadow`}
-                >
-                    <div className='flex items-center space-x-2'>
-                        <span 
-                            className={`w-3 h-3 flex rounded-full ${getDotColor(metric.type)}`}
-                        ></span>
-                        <span>{metric.type}</span>
-                    </div>
-                    <div className='flex items-center'>{metric.value}</div>
-                    <div className='flex items-center text-xs'>
-                        {formatDate(metric.date)}
-                    </div>
-                </div>
-            ))}
         </div>       
     );
 };
